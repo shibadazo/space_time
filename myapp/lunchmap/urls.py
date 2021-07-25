1746f142afafe7e1fe9463d93df471d1876660a3
shibadazo <s1922062@stu.musashino-u.ac.jp>
2021-07-26 02:07:13 +0900

    07-26-02-06

--- myapp/lunchmap/urls.py
+++ myapp/lunchmap/urls.py
@@ -6,4 +6,6 @@ app_name = 'lunchmap'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('create/', views.CreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.UpdateView.as_view(), name='update'),
 ]
--- myapp/lunchmap/views.py
+++ myapp/lunchmap/views.py
@@ -1,8 +1,17 @@
+from django.urls import reverse_lazy
 from django.views import generic
-from .models import Category, Shop
+from .models import Shop
 
 class IndexView(generic.ListView):
     model = Shop
 
-class DatailView(generic.DetailView):
+class DetailView(generic.DetailView):
     model = Shop
+
+class CreateView(generic.edit.CreateView):
+    model = Shop
+    fields = '__all__'
+
+class UpdateView(generic.edit.UpdateView):
+    model = Shop
+    fields = '__all__'
