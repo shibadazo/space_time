86f3f2fc1b5c7becb3d122e67f8b6ec20baba40c
shibadazo <s1922062@stu.musashino-u.ac.jp>
2022-04-15 21:05:48 +0900

    undo

--- myapp/lunchmap/urls.py
+++ myapp/lunchmap/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('create/', views.CreateView.as_view(), name='create'),
     path('<int:pk>/update/', views.UpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
-    path('', views.IndexView.as_view(), name='shuffle'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete')
 ]
